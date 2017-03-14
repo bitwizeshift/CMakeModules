8e22e68c68e0a2fcb9eb0881b73db5edf44044d6
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-03-14 00:57:23 -0400

    Updated git build information to allow filtering by PATHSPEC

--- modules/GitBuildInformation.cmake
+++ modules/GitBuildInformation.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 2.6.3)
+cmake_minimum_required(VERSION 3.1)
 
 include(CMakeParseArguments)
 
@@ -16,32 +16,51 @@ include(CMakeParseArguments)
 #                            [PATCH_VARIABLE <variable>]
 #                            [BUILD_VARIABLE <variable>]
 #                            [BRANCH_VARIABLE <variable>]
-#                            [HASH <hash>] )
+#                            [REF <hash>]
+#                            [PATHSPECS <specs>...)
 #
-#     MINOR_VARIABLE <variable> - The variable to store the minor number
-#     PATCH_VARIABLE <variable> - The variable to store the patch number
-#     BUILD_VARIABLE <variable> - The variable to store the build number
+#     MINOR_VARIABLE <variable>  - The variable to store the minor number
+#     PATCH_VARIABLE <variable>  - The variable to store the patch number
+#     BUILD_VARIABLE <variable>  - The variable to store the build number
 #     BRANCH_VARIABLE <variable> - The variable to store the minor number
-#     HASH <hash> - The git hash of the most recent major revision
+#     REF <hash>                 - The git reference of the most recent major revision.
+#                                  This may be an identifier, tag, or hash
+#     PATHSPECS <specs>...       - The path specs to use for identifiers
 #
-macro(git_build_information )
+macro(git_build_information)
 
-  set(__single_args MINOR_VARIABLE PATCH_VARIABLE BUILD_VARIABLE BRANCH_VARIABLE HASH)
-  cmake_parse_arguments("GIT" "" "${__single_args}" "" "${ARGN}")
+  set(__single_args MINOR_VARIABLE PATCH_VARIABLE BUILD_VARIABLE BRANCH_VARIABLE REF)
+  set(__multi_args PATHSPECS)
+  cmake_parse_arguments("GIT" "" "${__single_args}" "${__multi_args}" "${ARGN}")
 
-  if( NOT GIT_HASH )
-    message(FATAL_ERROR "git_build_information: HASH not specified")
+  if( NOT GIT_REF )
+    message(FATAL_ERROR "git_build_information: REF not specified")
   endif()
 
   if( NOT GIT_EXECUTABLE_PATH )
     find_program(GIT_EXECUTABLE_PATH git)
   endif()
 
-  if( GIT_EXECUTABLE_PATH )
+  set("${GIT_MINOR_VARIABLE}" "${GIT_MINOR_VARIABLE}-NOTFOUND")
+  set("${GIT_PATCH_VARIABLE}" "${GIT_PATCH_VARIABLE}-NOTFOUND")
+  set("${GIT_BUILD_VARIABLE}" "${GIT_BUILD_VARIABLE}-NOTFOUND")
+  set("${GIT_BRANCH_VARIABLE}" "${GIT_BRANCH_VARIABLE}-NOTFOUND")
 
-    ############################## MINOR VERSION #############################
+  if( NOT GIT_EXECUTABLE_PATH )
+    return()
+  endif()
 
-    execute_process(COMMAND "${GIT_EXECUTABLE_PATH}" rev-list --merges --count ${GIT_HASH}..HEAD
+  if( NOT GIT_REF )
+    set(_git_ref ${GIT_REF}..HEAD)
+  else()
+    set(_git_ref HEAD)
+  endif()
+
+  ############################## MINOR VERSION #############################
+
+  if( GIT_MINOR_VARIABLE )
+
+    execute_process(COMMAND "${GIT_EXECUTABLE_PATH}" rev-list --merges --count ${_git_ref} -- ${GIT_PATHSPECS}
                     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
                     OUTPUT_VARIABLE "${GIT_MINOR_VARIABLE}"
                     ERROR_VARIABLE "${_error}"
@@ -52,11 +71,13 @@ macro(git_build_information )
       message(FATAL_ERROR "git_build_information: Error retrieving minor revision. ${_error}")
     endif()
 
-    string(STRIP "${${GIT_MINOR_VARIABLE}}" "${GIT_MINOR_VARIABLE}")
+  endif()
 
-    ############################## PATCH VERSION #############################
+  ############################## PATCH VERSION #############################
 
-    execute_process(COMMAND "${GIT_EXECUTABLE_PATH}" rev-list --min-parents=2 --max-count=1 ${GIT_HASH}..HEAD
+  if( GIT_PATCH_VARIABLE )
+
+    execute_process(COMMAND "${GIT_EXECUTABLE_PATH}" rev-list --min-parents=2 --max-count=1 ${_git_ref} -- ${GIT_PATHSPECS}
                     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
                     OUTPUT_VARIABLE _recent_merge_commit
                     ERROR_VARIABLE "${_error}"
@@ -64,10 +85,10 @@ macro(git_build_information )
                     ERROR_STRIP_TRAILING_WHITESPACE )
 
     if( "${_recent_merge_commit}" STREQUAL "" )
-      set(_recent_merge_commit ${GIT_HASH})
+      set(_recent_merge_commit ${GIT_REF})
     endif()
 
-    execute_process(COMMAND "${GIT_EXECUTABLE_PATH}" rev-list --count ${_recent_merge_commit}..HEAD
+    execute_process(COMMAND "${GIT_EXECUTABLE_PATH}" rev-list --count ${_recent_merge_commit}..HEAD -- ${GIT_PATHSPECS}
                     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
                     OUTPUT_VARIABLE "${GIT_PATCH_VARIABLE}"
                     ERROR_VARIABLE "${_error}"
@@ -80,7 +101,11 @@ macro(git_build_information )
       message(FATAL_ERROR "git_build_information: Error retrieving patch revision. ${_error}")
     endif()
 
-    ############################## BUILD VERSION #############################
+  endif()
+
+  ############################## BUILD VERSION #############################
+
+  if( GIT_BUILD_VARIABLE )
 
     execute_process(COMMAND "${GIT_EXECUTABLE_PATH}" rev-list --count HEAD
                     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
@@ -93,7 +118,11 @@ macro(git_build_information )
       message(FATAL_ERROR "git_build_information: Error retrieving build number. ${_error}")
     endif()
 
-    ############################## BRANCH NAME ###############################
+  endif()
+
+  ############################## BRANCH NAME ###############################
+
+  if( GIT_BRANCH_VARIABLE )
 
     execute_process(COMMAND "${GIT_EXECUTABLE_PATH}" rev-parse --abbrev-ref HEAD
                     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
@@ -102,16 +131,9 @@ macro(git_build_information )
                     OUTPUT_STRIP_TRAILING_WHITESPACE
                     ERROR_STRIP_TRAILING_WHITESPACE )
 
-
     if( _error )
       message(FATAL_ERROR "git_build_information: Error retrieving build number. ${_error}")
     endif()
-
-  else()
-
-    set("${GIT_MINOR_VARIABLE}" "GIT_MINOR_VARIABLE-NOTFOUND")
-    set("${GIT_PATCH_VARIABLE}" "GIT_PATCH_VARIABLE-NOTFOUND")
-    set("${GIT_BUILD_VARIABLE}" "GIT_BUILD_VARIABLE-NOTFOUND")
 
   endif()
 
