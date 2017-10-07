eea1a10a3939cd7a2e14d35232cc1f3852bb606e
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-10-07 16:40:32 -0400

    Updated git rev-list to not use '..' path
    This may have been a bug triggered in older versions of git, causing
    spurious failures on Travis CI

--- modules/GitBuildInformation.cmake
+++ modules/GitBuildInformation.cmake
@@ -63,7 +63,7 @@ macro(git_build_information)
 
   if( GIT_PATCH_VARIABLE )
 
-    execute_process(COMMAND "${GIT_EXECUTABLE_PATH}" rev-list --count ${GIT_REF}..HEAD -- ${GIT_PATHSPECS}
+    execute_process(COMMAND "${GIT_EXECUTABLE_PATH}" rev-list --count ${GIT_REF} HEAD -- ${GIT_PATHSPECS}
                     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
                     OUTPUT_VARIABLE "${GIT_PATCH_VARIABLE}"
                     ERROR_VARIABLE "_error"
